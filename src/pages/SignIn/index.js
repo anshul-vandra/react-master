16882e62ce73d60cb7b1103ba887b07012f13a59
anshul.vandra <anshul.vandra@agileinfoways.com>
2022-08-24 16:28:26 +0530

    debouncing added

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { loginAction } from '../../Redux/AuthSlice';
+// import { debounce } from '../../utils';
 import './signin.scss';
 
 const SignIn = () => {
@@ -13,6 +14,11 @@ const SignIn = () => {
             .then(res => navigate('/dashboard'))
     }
 
+    // const onChangeFun = (e) => {
+    //     console.log('e.target.value: ', e);
+    // }
+    // const optimizedFn = debounce(onChangeFun);
+
     return (
         <>
             <div>
@@ -20,6 +26,7 @@ const SignIn = () => {
             </div>
             <br />
             <div style={{ display: 'flex', justifyContent: 'center' }}>
+                {/* <input onChange={(e) => { optimizedFn(e.target.value) }} /> */}
                 <button type="button" onClick={handleClick}>Login Test</button>
             </div>
         </>
--- src/utils/index.js
+++ src/utils/index.js
@@ -44,3 +44,14 @@ export const decrypt = param => {
     else
         return ''
 }
+export const debounce = (func) => {
+    let timer;
+    return function (...args) {
+        const context = this;
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(() => {
+            timer = null;
+            func.apply(context, args);
+        }, 500);
+    };
+};
